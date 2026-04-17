b14cdc92d5fb074556c8332d2d9b08d8358b7b92
vaibhav88614 <vaibhav88614@users.noreply.github.com>
2026-04-17 23:14:09 +0530

    updated

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState, useCallback } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Plus,
